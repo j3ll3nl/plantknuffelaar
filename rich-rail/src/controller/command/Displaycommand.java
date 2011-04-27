38be4e232bac8f1cedd66f03e758ca4b9cbbbe6a
j3ll3nl@gmail.com <j3ll3nl@gmail.com@c7a68891-1132-7a85-1aff-266986999077>
2011-04-27 17:45:00 +0000

    Minder if statements. Altijd beter

--- rich-rail/src/controller/command/Displaycommand.java
+++ rich-rail/src/controller/command/Displaycommand.java
@@ -9,39 +9,27 @@ public class Displaycommand implements CommandInterface{
 	
 	@Override
 	public CommandResult execute(String[] cmd) throws Exception {
-		if (cmd[1].equals("graphicdisplay")|| cmd[1].equals("textlog")) {
-			display(cmd);
+		if (cmd[1].equals("graphicdisplay")) {
+			display(cmd,new GraphicDisplay());
+		}else if(cmd[1].equals("textlog")){
+			display(cmd,new TextLog());
 		}else {
 			throw new Exception("unknown type given");
 		}
 		return cresult;
 	}
 	
-	private void display(String[] cmd) throws Exception {
+	private void display(String[] cmd,Object newDisplay) throws Exception {
 		System.out.println("Displaycommand.display(" + cmd + ")");
 				
-		// Create new jframe
+		// Create new frame
 		PopUpJFrame popup = new PopUpJFrame();
 		// Create new display
+		popup.initGUI();
+		// Set the display in the new frame
+		popup.setDisplay(newDisplay);
+		cresult.setObject(newDisplay);
 		
-		if(cmd[1].equals("graphicdisplay")){
-			// new graphic display
-			GraphicDisplay newDisplay = new GraphicDisplay();
-			// Start the GUI
-			popup.initGUI();
-			// Set the display in the new jframe
-			popup.setDisplay(newDisplay);
-			cresult.setObject(newDisplay);
-		}else{
-			// new Textlog display
-			TextLog newDisplay = new TextLog();
-			// Start the GUI
-			popup.initGUI();
-			// Set the display in the new jframe
-			popup.setDisplay(newDisplay);
-			cresult.setObject(newDisplay);
-		}
-
 		cresult.setMessage("display \"" + cmd[1] + "\" created");
 		
 
